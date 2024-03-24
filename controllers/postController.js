3e74c81eead87d47fb706b45fce624d387b0056f
oyeyinkaojora <oyeyinkaojoro@gmail.com>
2024-03-24 13:13:45 +0100

    Update and delete post api route added

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,9 +1,6 @@
 const asyncHandler = require("express-async-handler");
-const bcrypt = require("bcryptjs");
 const User = require("../models/userModel");
 const Post = require("../models/postModel");
-const jwt = require("jsonwebtoken");
-const { post } = require("../routes/userRoutes");
 
 const createPost = asyncHandler(async (req, res) => {
   const { title, content } = req.body;
@@ -62,4 +59,54 @@ const getPost = asyncHandler(async (req, res) => {
   res.status(200).json(post);
 });
 
-module.exports = { createPost, getPosts, getPost };
+const updatePost = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  const post = await Post.findById(req.params.id);
+
+  if (!post) {
+    res.status(404);
+    throw new Error("Post does not exists!");
+  }
+
+  if (post.author.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("You're not authorised to update this post");
+  }
+
+  const updatePost = await Post.findByIdAndUpdate(req.params.id, req.body);
+
+  res.status(200).json(updatePost);
+});
+
+const deletePost = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  const post = await Post.findById(req.params.id);
+
+  if (!post) {
+    res.status(404);
+    throw new Error("Post does not exists!");
+  }
+
+  if (post.author.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("You're not authorised to delete this post");
+  }
+
+  post.remove();
+
+  res.status(200).json(post);
+});
+
+module.exports = { createPost, getPosts, getPost, deletePost, updatePost };
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -4,10 +4,16 @@ const {
   createPost,
   getPosts,
   getPost,
+  updatePost,
+  deletePost,
 } = require("../controllers/postController");
 const { protect } = require("../middlewares/authMiddleware");
 
 router.route("/").get(protect, getPosts).post(protect, createPost);
-router.route("/:id").get(protect, getPost);
+router
+  .route("/:id")
+  .get(protect, getPost)
+  .delete(protect, deletePost)
+  .put(protect, updatePost);
 
 module.exports = router;
